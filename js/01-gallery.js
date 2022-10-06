bae65cdd8a85e7cb45df37cc635b8ae1cec82134
Mykola-Veres <veresnickolay@gmail.com>
2022-10-06 22:06:33 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,20 +22,25 @@ addGallery(galleryItems);
 
 galleryImage.addEventListener("click", handlerGallery);
 
+const instance = basicLightbox.create(`
+<img width="1280" height="860" src="">`,{
+  onShow: () => {
+    galleryImage.addEventListener("keydown", handlerRemove); 
+function handlerRemove (event) {
+  if (event.code === "Escape") {
+    instance.close(() => galleryImage.removeEventListener("keydown", handlerRemove))}}
+  },
+  // onClose: () => console.log('onClose', instance.element())
+});
+
 function handlerGallery (event) {
   event.preventDefault();
   const filterDatasetImg = event.target.dataset.source;
   if (!filterDatasetImg) return;
-
-const instance = basicLightbox.create(`
-<img width="1280" height="860" src="${filterDatasetImg}">`,{
-  onShow: instance => console.log('onShow', instance.element()),
-  onClose: instance => console.log('onClose', instance.element())
-});
+instance.element().querySelector("img").src = event.target.dataset.source;
 instance.show();
-
-galleryImage.addEventListener("keydown", handlerRemove); 
-function handlerRemove (event) {
-  if (event.code === "Escape") {
-    instance.close(() => galleryImage.removeEventListener("keydown", handlerRemove))}
-}};
+// galleryImage.addEventListener("keydown", handlerRemove); 
+// function handlerRemove (event) {
+//   if (event.code === "Escape") {
+//     instance.close(() => galleryImage.removeEventListener("keydown", handlerRemove))}}
+};
